3b39559b0c3c9bc3ee23d48ed5c1a78eea9c3268
Dion Tryban <github@diontryban.com>
2023-04-01 22:10:40 -0400

    Port to 1.19.4

---
+++ Common/src/main/java/com/diontryban/armor_visibility/mixin/client/ElytraLayerMixin.java
@@ -0,0 +1,56 @@
+/*
+ * This file is part of Armor Visibility.
+ * A copy of this program can be found at https://github.com/Trikzon/armor-visibility.
+ * Copyright (C) 2023 Dion Tryban
+ *
+ * Armor Visibility is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * Armor Visibility is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Armor Visibility. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.diontryban.armor_visibility.mixin.client;
+
+import com.diontryban.armor_visibility.client.ArmorVisibilityClient;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.RenderLayerParent;
+import net.minecraft.client.renderer.entity.layers.ElytraLayer;
+import net.minecraft.client.renderer.entity.layers.RenderLayer;
+import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ElytraLayer.class)
+public abstract class ElytraLayerMixin<T extends LivingEntity, M extends EntityModel<T>> extends RenderLayer<T, M> {
+    public ElytraLayerMixin(RenderLayerParent<T, M> renderLayerParent) {
+        super(renderLayerParent);
+    }
+
+    @Inject(at = @At("HEAD"), cancellable = true, method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/world/entity/LivingEntity;FFFFFF)V")
+    private void injectBeforeRender(
+            PoseStack poseStack,
+            MultiBufferSource bufferSource,
+            int i,
+            T livingEntity,
+            float f, float g, float h, float j, float k, float l,
+            CallbackInfo ci
+    ) {
+        if (ArmorVisibilityClient.config.keepElytraVisible) {
+            return;
+        }
+
+        ArmorVisibilityClient.maybeCancelRender(livingEntity, ci);
+    }
+}
---
+++ Forge/src/main/java/com/diontryban/armor_visibility/ArmorVisibilityForge.java
@@ -0,0 +1,36 @@
+/*
+ * This file is part of Armor Visibility.
+ * A copy of this program can be found at https://github.com/Trikzon/armor-visibility.
+ * Copyright (C) 2023 Dion Tryban
+ *
+ * Armor Visibility is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * Armor Visibility is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Armor Visibility. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.diontryban.armor_visibility;
+
+import com.diontryban.ash.api.modloader.forge.ForgeModLoader;
+import com.diontryban.armor_visibility.client.ArmorVisibilityClientForge;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(ArmorVisibility.MOD_ID)
+public class ArmorVisibilityForge {
+    public ArmorVisibilityForge() {
+        ForgeModLoader.registerMod(ArmorVisibility.MOD_ID, ModLoadingContext.get(), FMLJavaModLoadingContext.get());
+        DistExecutor.safeCallWhenOn(Dist.CLIENT, () -> ArmorVisibilityClientForge::new);
+    }
+}
